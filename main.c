7452efceb2bf859d6290426fa4d2056c827668eb
Ícaro Gonçalves <139063411+icarosg@users.noreply.github.com>
2024-12-14 10:18:34 -0300

    Adicionado função de comprimento

--- main.c
+++ main.c
@@ -17,6 +17,10 @@ double toneladasParaQuilogramas(double t) { return t * 1000; }
 // volume
 double litrosParaMililitros(double litros) { return litros * 1000; }
 double mililitrosParaLitros(double ml) { return ml / 1000; }
+double metrosCubicosParaLitros(double metrosCubicos) { return metrosCubicos * 1000; }
+double litrosParaMetrosCubicos(double litros) { return litros / 1000; }
+double centimetrosCubicosParaLitros(double cm3) { return cm3 / 1000.0; }
+double litrosParaCentimetrosCubicos(double litros) { return litros * 1000.0; }
 
 // temperatura
 double celsiusParaFahrenheit(double c) { return (c * 9 / 5) + 32; }
@@ -47,6 +51,110 @@ double bytesParaKilobytes(double bytes) { return bytes / 1024; }
 double kilobytesParaBytes(double kb) { return kb * 1024; }
 double kilobytesParaMegabytes(double kb) { return kb / 1024; }
 
+// Função separada para as conversões de volume
+void conversaoVolume()
+{
+    int opcao;
+    double valor;
+
+    printf("\nEscolha o tipo de conversao de volume:\n");
+    printf("1. Litros para Mililitros\n");
+    printf("2. Mililitros para Litros\n");
+    printf("3. Metros Cúbicos para Litros\n");
+    printf("4. Litros para Metros Cúbicos\n");
+    printf("5. Centímetros Cúbicos para Litros\n");
+    printf("6. Litros para Centímetros Cúbicos\n");
+    scanf("%d", &opcao);
+
+    switch (opcao)
+    {
+    case 1:
+        printf("Digite o valor em Litros: ");
+        scanf("%lf", &valor);
+        printf("%.2lf Litros = %.2lf Mililitros\n", valor, litrosParaMililitros(valor));
+        break;
+    case 2:
+        printf("Digite o valor em Mililitros: ");
+        scanf("%lf", &valor);
+        printf("%.2lf Mililitros = %.2lf Litros\n", valor, mililitrosParaLitros(valor));
+        break;
+    case 3:
+        printf("Digite o valor em Metros Cúbicos: ");
+        scanf("%lf", &valor);
+        printf("%.2lf Metros Cúbicos = %.2lf Litros\n", valor, metrosCubicosParaLitros(valor));
+        break;
+    case 4:
+        printf("Digite o valor em Litros: ");
+        scanf("%lf", &valor);
+        printf("%.2lf Litros = %.2lf Metros Cúbicos\n", valor, litrosParaMetrosCubicos(valor));
+        break;
+    case 5:
+        printf("Digite o valor em Centímetros Cúbicos: ");
+        scanf("%lf", &valor);
+        printf("%.2lf Centímetros Cúbicos = %.2lf Litros\n", valor, centimetrosCubicosParaLitros(valor));
+        break;
+    case 6:
+        printf("Digite o valor em Litros: ");
+        scanf("%lf", &valor);
+        printf("%.2lf Litros = %.2lf Centímetros Cúbicos\n", valor, litrosParaCentimetrosCubicos(valor));
+        break;
+    default:
+        printf("Opção inválida.\n");
+    }
+}
+
+// Função separada para as conversões de comprimento
+void conversaoComprimento()
+{
+    int opcao;
+    double valor;
+
+    printf("\nEscolha o tipo de conversao de comprimento: \n");
+    printf("1. Metros para centimetros\n");
+    printf("2. Metros para milimetros\n");
+    printf("3. Centimetros para metros\n");
+    printf("4. Centimetros para milimetros\n");
+    printf("5. Milimetros para metros\n");
+    printf("6. Milimetros para centimetros\n");
+    scanf("%d", &opcao);
+
+    switch (opcao)
+    {
+    case 1:
+        printf("Digite o valor em metros: ");
+        scanf("%lf", &valor);
+        printf("\n%.2lf Metros = %.2lf Centimetros\n", valor, metrosParaCentimetros(valor));
+        break;
+    case 2:
+        printf("Digite o valor em metros: ");
+        scanf("%lf", &valor);
+        printf("\n%.2lf Metros = %.2lf Milimetros\n", valor, metrosParaMilimetros(valor));
+        break;
+    case 3:
+        printf("Digite o valor em centimetros: ");
+        scanf("%lf", &valor);
+        printf("\n%.2lf Centimetros = %.2lf Metros\n", valor, centimetrosParaMetros(valor));
+        break;
+    case 4:
+        printf("Digite o valor em centimetros: ");
+        scanf("%lf", &valor);
+        printf("\n%.2lf Centimetros = %.2lf Milimetros\n", valor, centimetrosParaMilimetros(valor));
+        break;
+    case 5:
+        printf("Digite o valor em milimetros: ");
+        scanf("%lf", &valor);
+        printf("\n%.2lf milimetros = %.2lf Metros\n", valor, milimetrosParaMetros(valor));
+        break;
+    case 6:
+        printf("Digite o valor em milimetros: ");
+        scanf("%lf", &valor);
+        printf("\n%.2lf milimetros = %.2lf Centimetros\n", valor, milimetrosParaCentimetros(valor));
+        break;
+    default:
+        printf("Opcao invalida.\n");
+    }
+}
+
 int main()
 {
     int categoria;
@@ -54,13 +162,34 @@ int main()
     do
     {
         printf("\nEscolha a categoria de conversao:\n");
-
+        printf("1. Comprimento\n");
+        printf("2. Massa\n");
+        printf("3. Volume\n");
+        printf("4. Temperatura\n");
+        printf("5. Velocidade\n");
+        printf("6. Potencia\n");
+        printf("7. Area\n");
+        printf("8. Tempo\n");
+        printf("9. Armazenamento\n");
+        printf("0. Sair\n");
         scanf("%d", &categoria);
 
-        if (categoria == 0)
+        switch (categoria)
         {
+        case 1:
+            conversaoComprimento(); // Chama a função separada para comprimento
+            break;
+        case 2:
+            // Aqui iriam as opções de conversões de massa
+            break;
+        case 3:
+            conversaoVolume(); // Chama a função separada para volume
+            break;
+        case 0:
             printf("Saindo...\n");
             break;
+        default:
+            printf("Opção inválida.\n");
         }
 
     } while (categoria != 0);
